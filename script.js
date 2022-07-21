d1a7dd78bb62efe2e29d2207ffdf1c7f651a9e26
BDK27 <briansam195@gmail.com>
2022-07-21 16:51:53 +0800

    site section

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ let tabcontents = document.getElementsByClassName('tab-contents');
 
 function opentab(tabname){
     for (tablink of tablinks){
-        tablink.classList.remove('active-tab');
+        tablink.classList.remove('active-link');
     }
     
     for (tabcontent of tabcontents){
         tabcontent.classList.remove('active-tab');
     }
+    event.currentTarget.classList.add('active-link');
     document.getElementById(tabname).classList.add('active-tab');
 }
 
